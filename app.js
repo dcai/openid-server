19e46feedccacf1042e48ef9962fe145a1ab816d
Dongsheng Cai <d@tux.im>
2019-03-27 11:15:51 +0000

    fix: audit fix

--- app.js
+++ app.js
@@ -1,38 +1,56 @@
-const port = '9090';
+const port = '3005';
 const clientCallbackUrl = 'https://westpac-sso-app/callback';
 const clientId = 'westpac-sso-app';
-
 
 const express = require('express');
 const Provider = require('oidc-provider');
 
 const app = express();
 
-const clients = [{
+const clients = [
+  {
     client_id: clientId,
     grant_types: ['implicit'],
     response_types: ['id_token'],
     redirect_uris: [clientCallbackUrl],
-    token_endpoint_auth_method: 'none'
-}];
-
+    token_endpoint_auth_method: 'none',
+  },
+];
 
 const providerConfig = {
-    claims: {
-        email: ['email', 'email_verified'],
-        phone: ['phone_number', 'phone_number_verified'],
-        profile: ['birthdate', 'family_name', 'gender', 'given_name', 'locale', 'middle_name', 'name', 'nickname', 'picture', 'preferred_username', 'profile', 'updated_at', 'website', 'zoneinfo']
-    },
-    features: {
-        clientCredentials: true,
-        introspection: true,
-        sessionManagement: true
-    }
+  claims: {
+    email: ['email', 'email_verified'],
+    phone: ['phone_number', 'phone_number_verified'],
+    profile: [
+      'birthdate',
+      'family_name',
+      'gender',
+      'given_name',
+      'locale',
+      'middle_name',
+      'name',
+      'nickname',
+      'picture',
+      'preferred_username',
+      'profile',
+      'updated_at',
+      'website',
+      'zoneinfo',
+    ],
+  },
+  features: {
+    clientCredentials: true,
+    introspection: true,
+    sessionManagement: true,
+  },
 };
 
 const oidc = new Provider(`http://localhost:${port}`, providerConfig);
-oidc.initialize({clients}).then(function () {
-    app.use('/', oidc.callback);
-    app.listen(port);
-
+oidc.initialize({ clients }).then(function() {
+  app.use('/', oidc.callback);
+  app.listen(port, () => {
+    console.log(
+      `oidc-provider listening on port ${port}, check http://localhost:${port}/.well-known/openid-configuration`,
+    );
+  });
 });
